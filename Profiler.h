5fc85c057480e8bd46c76b863412a78de5e91e27
eari359 <eari359@fake.com>
2020-08-02 01:02:08 +0200

    added ProfileResult to namespaces, formating

--- Profiler.h
+++ Profiler.h
@@ -16,112 +16,113 @@
 #define PROFILE_SCOPE(name)
 #endif
 
-struct ProfileResult
-{
-    char const* name;
-    long long start;
-    long long end;
-    uint32_t thread_id;
-};
-
 namespace profiler
 {
-   class Instrumentor
-   {
-   public:
-      static Instrumentor& Instance() {
-         static Instrumentor instance;
-         return instance;
-      }
+	struct ProfileResult
+	{
+		char const* name;
+		long long start;
+		long long end;
+		uint32_t thread_id;
+	};
 
-      void beginSession(char const* file_path = "profile.json")
-      {
-         output_stream_.open(file_path);
-         writeHeader();
-      }
+	class Instrumentor
+	{
+		public:
+			static Instrumentor& Instance()
+			{
+				static Instrumentor instance;
+				return instance;
+			}
 
-      void endSession()
-      {
-         writeFooter();
-         output_stream_.close();
-         first_profile = true;
-      }
+			void beginSession(char const* file_path = "profile.json")
+			{
+				output_stream_.open(file_path);
+				writeHeader();
+			}
 
-      void writeProfile(ProfileResult const& result)
-      {
-         assert(output_stream_.is_open());
+			void endSession()
+			{
+				writeFooter();
+				output_stream_.close();
+				first_profile = true;
+			}
 
-         if (!first_profile) {
-            output_stream_ << ",";
-         }
+			void writeProfile(ProfileResult const& result)
+			{
+				assert(output_stream_.is_open());
 
-         first_profile = false;
+				if (!first_profile) {
+					output_stream_ << ",";
+				}
 
-         std::string name = result.name;
-         std::replace(name.begin(), name.end(), '"', '\'');
+				first_profile = false;
 
-         output_stream_ << '{';
-         output_stream_ << R"("cat":"function",)";
-         output_stream_ << R"("dur":)" << (result.end - result.start) << ',';
-         output_stream_ << R"("name":")" << name << R"(",)";
-         output_stream_ << R"("ph":"X",)";
-         output_stream_ << R"("pid":0,)";
-         output_stream_ << R"("tid":)" << result.thread_id << ',';
-         output_stream_ << R"("ts":)" << result.start;
-         output_stream_ << '}';
+				std::string name = result.name;
+				std::replace(name.begin(), name.end(), '"', '\'');
+
+				output_stream_ << '{';
+				output_stream_ << R"("cat":"function",)";
+				output_stream_ << R"("dur":)" << (result.end - result.start) << ',';
+				output_stream_ << R"("name":")" << name << R"(",)";
+				output_stream_ << R"("ph":"X",)";
+				output_stream_ << R"("pid":0,)";
+				output_stream_ << R"("tid":)" << result.thread_id << ',';
+				output_stream_ << R"("ts":)" << result.start;
+				output_stream_ << '}';
 
 
-         output_stream_.flush();
-      }
+				output_stream_.flush();
+			}
 
-      void writeHeader()
-      {
-         assert(output_stream_.is_open());
-         output_stream_ << "{\"otherData\": {},\"traceEvents\":[";
-      }
+			void writeHeader()
+			{
+				assert(output_stream_.is_open());
+				output_stream_ << "{\"otherData\": {},\"traceEvents\":[";
+			}
 
-      void writeFooter()
-      {
-         assert(output_stream_.is_open());
-         output_stream_ << "]}";
-         output_stream_.flush();
-      }
+			void writeFooter()
+			{
+				assert(output_stream_.is_open());
+				output_stream_ << "]}";
+				output_stream_.flush();
+			}
 
-   protected:
-      Instrumentor() {}
+		protected:
+			Instrumentor() {}
 
-   private:
-      std::ofstream output_stream_;
-      bool first_profile{ true };
-   };
+		private:
+			std::ofstream output_stream_;
+			bool first_profile{ true };
+	};
 
-   class InstrumentationTimer
-   {
-      using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
+	class InstrumentationTimer
+	{
+		using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
 
-   public:
-      explicit InstrumentationTimer(char const* name)
-         : name_{ name }
-      {
-         start_ = std::chrono::system_clock::now();
-      }
+		public:
+		explicit InstrumentationTimer(char const* name)
+			: name_{ name }
+		{
+			start_ = std::chrono::system_clock::now();
+		}
 
-      ~InstrumentationTimer()
-      {
-         long long start =
-            std::chrono::time_point_cast<std::chrono::microseconds>(start_)
-            .time_since_epoch().count();
-         long long end =
-            std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now())
-            .time_since_epoch().count();
+		~InstrumentationTimer()
+		{
+			long long start =
+				std::chrono::time_point_cast<std::chrono::microseconds>(start_)
+				.time_since_epoch().count();
+			long long end =
+				std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now())
+				.time_since_epoch().count();
 
-         uint32_t thread_id = std::hash<std::thread::id>{}(std::this_thread::get_id());
+			uint32_t thread_id = std::hash<std::thread::id>{}(std::this_thread::get_id());
 
-         Instrumentor::Instance().writeProfile({ name_, start, end, thread_id });
-      }
+			Instrumentor::Instance().writeProfile({ name_, start, end, thread_id });
+		}
 
-   private:
-      TimePoint start_;
-      char const* name_;
-   };
+		private:
+		TimePoint start_;
+		char const* name_;
+	};
 }
